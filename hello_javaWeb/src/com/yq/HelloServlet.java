2d592dca2e4252894ec5ba091df002fdc72206b0
zhangsan <zhangsan@163.com>
2020-05-13 19:51:49 +0800

    修改了Servlet

--- hello_javaWeb/src/com/yq/HelloServlet.java
+++ hello_javaWeb/src/com/yq/HelloServlet.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 public class HelloServlet extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-
+        System.out.println("doPost............");
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
