5c9d4eb709d4a49582a13c476305da1feb186e42
zhangsan <zhangsan@163.com>
2020-05-13 19:31:42 +0800

    111111111

--- hello_javaWeb/src/com/yq/HelloServlet.java
+++ hello_javaWeb/src/com/yq/HelloServlet.java
@@ -12,5 +12,6 @@ public class HelloServlet extends javax.servlet.http.HttpServlet {
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         Gson g=new Gson();
         response.getWriter().println(g.toJson(new User()));
+        System.out.println("11111111111111");
     }
 }
