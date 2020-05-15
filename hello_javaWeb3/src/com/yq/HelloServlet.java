b331a39aa7d847b0ea496f7fd787e6692ee66e84
zhangsan <zhangsan@163.com>
2020-05-15 09:39:31 +0800

    aaaaaaaaaaaaa

--- hello_javaWeb3/src/com/yq/HelloServlet.java
+++ hello_javaWeb3/src/com/yq/HelloServlet.java
@@ -14,6 +14,6 @@ public class HelloServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        response.getWriter().println("Hello Servlet!!!!");
     }
 }
